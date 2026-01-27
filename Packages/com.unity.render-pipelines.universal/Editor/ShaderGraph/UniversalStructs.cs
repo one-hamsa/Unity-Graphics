b7a34b94b8ffcbf9a1ae8e85a5b9a7af7f214a5a
Niv Fisher <niv@onehamsa.com>
2026-01-27 11:51:58 +0200

    Revert "moved some things from oculus branch so shaders will compile"
    This reverts commit 8141e1c09971145bf08018d776d8f377ca4cf6f8.

--- Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/UniversalStructs.cs
+++ Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/UniversalStructs.cs
@@ -31,8 +31,6 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
                 UniversalStructFields.Varyings.probeOcclusion,
                 UniversalStructFields.Varyings.fogFactorAndVertexLight,
                 UniversalStructFields.Varyings.shadowCoord,
-                UniversalStructFields.Varyings.curPositionCS,
-                UniversalStructFields.Varyings.prevPositionCS,
                 StructFields.Varyings.instanceID,
                 UniversalStructFields.Varyings.stereoTargetEyeIndexAsBlendIdx0,
                 UniversalStructFields.Varyings.stereoTargetEyeIndexAsRTArrayIdx,
